0ddaf08509442987d34f99bd06ad8fb0037a82dc
Aaron Meurer <asmeurer@gmail.com>
2011-10-12 19:12:36 -0600

    Add skeleton structure to the file

---
+++ neuralnet.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+"""
+Project 1. Aaron Meurer.
+
+This is a multilayer perceptron neural network.  The project is to experiment
+with using it to solve the subset sum problem, which is NP-complete.
+
+"""
+
+import sys
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
