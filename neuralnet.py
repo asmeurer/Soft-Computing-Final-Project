fba50157ef065fa9abe5ad08465ae7a94c1df029
Aaron Meurer <asmeurer@gmail.com>
2011-10-12 20:39:05 -0600

    Read in the problems in the network

--- neuralnet.py
+++ neuralnet.py
@@ -11,7 +11,20 @@ with using it to solve the subset sum problem, which is NP-complete.
 import sys
 
 def main():
-    pass
+    with open("problems", 'r') as file:
+        problemstxt = file.read()
+
+    problems = []
+
+    for line in problemstxt.split('\n'):
+        if not line:
+            # Handle blank lines at the end of the file
+            continue
+
+        problems.append(eval(line))
+
+    print problems
+
 
 if __name__ == "__main__":
     main()
