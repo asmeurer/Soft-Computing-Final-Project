fc518f7e5c708b792b358c682cf72abf54b77650
Aaron Meurer <asmeurer@gmail.com>
2011-10-12 20:01:22 -0600

    Start a problem generating script

---
+++ generate_problems.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+"""
+This generates the subset problems for input to the neural network.
+"""
+
+import argparse
+import random
+
+# TODO: Refactor these into command line arguments:
+
+setsize = 10
+number_of_sets = 100
+
+parser = argparse.ArgumentParser(description="This generates the subset "
+    "problems for input to the neural network.")
+
+parser.add_argument('-s', '--seed', metavar='N', type=int, nargs=1,
+                   help='The random seed used to generate the output.')
+
+args = parser.parse_args()
+
+def main():
+    for i in range(number_of_sets):
+        print [(-1)**random.randint(0, 1)*random(1, 10) for i in range(setsize)]
+    return
+
+if __name__ == "__main__":
+    main()
