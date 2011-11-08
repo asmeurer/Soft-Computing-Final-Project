a3aec68054ad70da9052c0d4fb07d10c1bd9f6f2
Aaron Meurer <asmeurer@gmail.com>
2011-11-07 21:57:13 -0700

    Do some more work on the algorithm

--- neuralnet.py
+++ neuralnet.py
@@ -7,13 +7,20 @@ This is a multilayer perceptron neural network.  The project is to experiment
 with using it to solve the subset sum problem, which is NP-complete.
 
 """
-
+from __future__ import division
 import sys
+import random
+import math
 
 NODES = [10, 4, 1]
+eps = 0.1
 
 def main():
     problems = get_problems()
+    W = init_weights()
+    while objective() < eps:
+        break
+
 
 def get_problems():
     with open("problems", 'r') as file:
@@ -30,6 +37,29 @@ def get_problems():
         problems.append((set, int(has_subsets)))
 
     return problems
+
+def init_weights(seed=None, max=1):
+    if seed:
+        random.seed(seed)
+
+    W = []
+    for nodelen in NODES:
+        W.append([random.random()*max for i in xrange(nodelen)])
+
+    return W
+
+def objective():
+    pass
+
+def energy():
+    return
+
+
+def sigmoid(h, k=1):
+    """
+    Return 1/(1 + exp(-k*h)).
+    """
+    return 1/(1 + math.exp(-k*h))
 
 if __name__ == "__main__":
     main()
