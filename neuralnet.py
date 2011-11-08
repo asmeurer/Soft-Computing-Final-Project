6bbd21431f3b7a8e403af3dad8d30a68569b141e
Aaron Meurer <asmeurer@gmail.com>
2011-11-07 22:24:14 -0700

    Do more work on the algorithm

--- neuralnet.py
+++ neuralnet.py
@@ -14,12 +14,14 @@ import math
 
 NODES = [10, 4, 1]
 eps = 0.1
+eta = 1
 
 def main():
     problems = get_problems()
     W = init_weights()
-    while objective() < eps:
-        break
+    while objective(patterns) < eps:
+        for pattern in patterns:
+            break
 
 
 def get_problems():
@@ -48,18 +50,34 @@ def init_weights(seed=None, max=1):
 
     return W
 
-def objective():
+def objective(patterns):
     pass
 
-def energy():
-    return
-
+def error(pattern):
+    pass
 
 def sigmoid(h, k=1):
     """
     Return 1/(1 + exp(-k*h)).
     """
     return 1/(1 + math.exp(-k*h))
+
+def sigmoiddiff(fh, k=1):
+    """
+    Return d/dh(f(h)) in terms of f(h), where f(h) = 1/(1 + exp(-k*h)).
+    """
+    return k*fh*(1 - fh)
+
+def activation(pattern, W, layer, node):
+    if layer == -1:
+        return sum(wij*opi for wij, opi in zip(W[layer], pattern[0])
+    return sum(wij*opi for wij, opi in zip(
+
+def errorsignal(pattern, W, layer, node):
+    if layer == len(NODES) - 1:
+        # Outer layer
+        o = output(pattern, W, layer, node)
+        return sigmoiddiff(o)*(pattern[1] - o)
 
 if __name__ == "__main__":
     main()
