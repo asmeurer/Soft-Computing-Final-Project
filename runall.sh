181a708d306587355017130517ec14d2ddc416f4
Aaron Meurer <asmeurer@gmail.com>
2011-11-16 12:11:31 -0700

    Fix the runall.sh script

--- runall.sh
+++ runall.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/sh
+#!/sw/bin/bash
+# We have to run bash >= 4 for the for i in  syntax.
 
 # Run the neural net with [10, 10, 10, 1] ... [80, 80, 80, 1] indefinitely
 
@@ -8,6 +9,7 @@ do
     ./generate_patterns.py patterns_test
     for i in {10..80..10}
     do
+        echo "NODES = [$i $i $i 1]"
         ./neuralnet.py patterns -t patterns_test -N $i $i $i 1
     done
 done
